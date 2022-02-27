3ad9d3664d6d5f5426417aa8fd0c344a2882e745
Sverine <severine.sylvan@gmail.com>
2022-02-27 14:16:09 +0100

    added word posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default ({data}) => {
     <Seo title="Home" />
     <div>
       <h1>Severine's thoughts</h1>
-      <h4>{data.allMarkdownRemark.totalCount}</h4>
+      <h4>{data.allMarkdownRemark.totalCount} posts</h4>
       {
         data.allMarkdownRemark.edges.map(({node}) => (
           <div key={node.id}>
